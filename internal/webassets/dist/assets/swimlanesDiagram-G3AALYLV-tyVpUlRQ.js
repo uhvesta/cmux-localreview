1233d8ab555cc43d38ed966a045bf825ea25394a
Avesta Barzegar <a3barzeg@uwaterloo.ca>
2026-07-28 09:17:47 -0700

    build: include staged embedded web assets

---
+++ internal/webassets/dist/assets/swimlanesDiagram-G3AALYLV-tyVpUlRQ.js
@@ -0,0 +1,8 @@
+import{n as e}from"./chunk-Y2CYZVJY-DsF7k-Jl.js";import"./src-BwpDq_i0.js";import"./chunk-WYO6CB5R-Dh5_ANAG.js";import"./chunk-ICXQ74PX-BZDXzqml.js";import"./chunk-HOUHSVGY-PQvSUXgB.js";import"./chunk-Q4XR5HBZ-B-BjBZdV.js";import"./chunk-7BUUIJ7U-Bb538aSH.js";import"./chunk-OGEWGWER-BKO4GKQ7.js";import"./chunk-32BRIVSS-tp132wik.js";import"./chunk-XXDRQBXY-DTQNpOka.js";import"./chunk-VR4S4FIN-CtRld8aN.js";import"./chunk-C7G6YPKG-B-bmLvzQ.js";import"./chunk-ZGVPDNZ5-DgQHEVbh.js";import"./chunk-52WLFC77-C0G1chF_.js";import"./chunk-FWX5IMBZ-BnM9x_Q6.js";import"./chunk-ZIRB5QZD-C6fEPe3t.js";import{r as t,t as n}from"./chunk-PUDLZKDR-BuhPTLh5.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
